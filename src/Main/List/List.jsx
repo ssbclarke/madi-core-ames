5d0f6e2319ee71f74bae6b22d4f28152a2f6fd3f
James Villarrubia <james.villarrubia@gmail.com>
2022-07-22 19:59:26 -0400

    adds collapsible

--- src/Main/List/List.jsx
+++ src/Main/List/List.jsx
@@ -1,45 +1,79 @@
 import React, { useContext, useMemo } from "react";
-import { CheckboxGroup } from './EntityFilter/EntityCheckboxes';
-import { generateList } from './generate-list';
+import { CheckboxGroup } from "./EntityFilter/EntityCheckboxes";
+import { generateList } from "./generate-list";
 import { EntityFilterContext } from "./EntityFilter/FilterContext";
 import { TagFilterContext } from "../TagFilter/TagContext";
 
-const list = generateList(2000)
 
+
+
+const list = generateList(20);
 
 // this SHOULD be a search filter of some sort.
 // but we'll do local tag filtering for now.
-function filterList({entityFilterStore, tagFilterStore, list}){
+function filterList({ entityFilterStore, tagFilterStore, list }) {
   // let tagTypes = Object.keys(tagFilterStore)
   // let tags = []
   // tagTypes.forEach(type=>{ tags = [...tags, ...tagFilterStore[type]]})
-  let filteredList = list
-    .filter(x=>entityFilterStore[x.type])
+  let filteredList = list.filter((x) => entityFilterStore[x.type]);
 
-  return filteredList
+  return filteredList;
 }
 
 const ComplexList = () => {
-  const { entityFilterStore, setEntityFilterStore } = useContext(EntityFilterContext)
-  const { tagFilterStore, setTagFilterStore } = useContext(TagFilterContext)
+  const { entityFilterStore, setEntityFilterStore } =
+    useContext(EntityFilterContext);
+  const { tagFilterStore, setTagFilterStore } = useContext(TagFilterContext);
 
   return (
     <div className="overflow-x-auto w-full">
-        <CheckboxGroup/>
-        <table className="table w-full">
-            <tbody>
-              {filterList({entityFilterStore, tagFilterStore, list}).map(item => ( 
-                  <tr key={item.id}>
-                      <td>{item.id}</td>
-                      <td>{item.firstname}</td>
-                      <td>{item.lastname}</td>
-                      <td>{item.type}</td>
-                  </tr>
-              ))}
-            </tbody>     
-        </table>
+      <CheckboxGroup />
+      {filterList({ entityFilterStore, tagFilterStore, list }).map((item) => (
+        <div
+          key={item.id}
+          tabindex="0"
+          class="collapse collapse-arrow 
+            bg-base-100 
+            mb-2
+          ">
+          <input type="checkbox" class="peer" />
+          <div class="collapse-title 
+            border-l-8 border-l-base-100
+            transition-color ease-out duration-300
+            peer-hover:border-l-indigo-500 
+            peer-checked:border-l-indigo-500 
+            peer-checked:bg-indigo-200
+            peer-checked:text-base-content">
+              Click me to show/hide content
+          </div>
+          <div class="collapse-content 
+            border-l-8 border-l-base-100
+            transition-color ease-out duration-300
+            peer-checked:bg-indigo-200
+            peer-checked:text-base-content
+            peer-checked:border-l-indigo-500 
+            "> 
+            <p>{item.id}</p>
+            <p>{item.firstname}</p>
+            <p>{item.lastname}</p>
+            <p>{item.type}</p>
+            </div>
+        </div>
+      ))}
     </div>
   );
 };
+
+// <table className="table w-full">
+//     <tbody>
+//     <tr key={item.id}>
+//     <p>{item.id}</p>
+//     <p>{item.firstname}</p>
+//     <p>{item.lastname}</p>
+//     <p>{item.type}</p>
+// </tr>
+//     </tbody>
+
+// </table>
 
 export default ComplexList;
