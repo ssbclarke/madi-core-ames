9c80ec8cdba29d2e578e5fc0b01681f55c63b4cd
James Villarrubia <james.villarrubia@gmail.com>
2022-08-26 10:03:52 -0400

    adds parser

---
+++ parser/tests/isAllowedByRobotsTxt.test.js
@@ -0,0 +1,29 @@
+import assert from 'assert'
+import rewire from 'rewire'
+
+describe('\'isAllowedByRobotsTxt\' function', () => {
+  it('error with null url', () => {
+    assert.ok(false);
+  });
+  it('error with null url', () => {
+    assert.ok(false);
+  });
+  it('standardize the url', ()=> {
+    assert.ok(false);
+  });
+  it('gets root domain',()=>{
+    assert.ok(false);
+  });
+  it('stores locally',()=>{
+    assert.ok(false);
+  });
+  it('fetches locally when previously stored',()=>{
+    assert.ok(false);
+  });
+  it('returns null if not there',()=>{
+    assert.ok(false);
+  });
+  it('stores null if not there',()=>{
+    assert.ok(false);
+  });
+});
