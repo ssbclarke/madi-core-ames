25dd6515326cfd81e76379ea6ceb70822c901a63
James Villarrubia <james.villarrubia@gmail.com>
2022-11-28 15:12:25 -0500

    no message

--- mural-scraper/src/converter/helpers/addArrowRelations.js
+++ mural-scraper/src/converter/helpers/addArrowRelations.js
@@ -1,27 +1,21 @@
-function addArrowRelations(db, raw){
-    raw.widgets.forEach(w=>{
-        if(w.type === 'arrow'){
-            let { endRefId, startRefId } = w
-            if(db[startRefId] && db[endRefId]){
-                let parent
-                let child
-                if (db[startRefId].type === 'takeaway'){
-                    parent = startRefId
-                    child = endRefId
-                }
-                if (db[endRefId].type === 'takeaway'){
-                    parent = endRefId
-                    child = startRefId
-                }
-                if(parent && child){
-                    // push the child to the right array
-                    db[parent][db[child].type] = db[parent][db[child].type] || []
-                    db[parent][db[child].type].push(db[child])
-                    //null out the child
-                    delete db[child]
-                }
-            }
-
+function addArrowRelations(arrows, db){
+    arrows.forEach(({startRefId, endRefId})=>{
+        let parent
+        let child
+        if (db[startRefId]?.type === 'takeaway'){
+            parent = startRefId
+            child = endRefId
+        }
+        if (db[endRefId]?.type === 'takeaway'){
+            parent = endRefId
+            child = startRefId
+        }
+        if(parent && child && db[parent] && db[child]){
+            // push the child to the right array
+            db[parent].children = db[parent]?.children || []
+            db[parent].children.push(child)
+            db[child].parents = db[child]?.parents || []
+            db[child].parents.push(parent)
         }
     })
     return db
--- mural-scraper/src/converter/helpers/converter.js
+++ mural-scraper/src/converter/helpers/converter.js
@@ -3,7 +3,7 @@ const addArrowRelations = require('./addArrowRelations')
 const populateMadiTypeField = require( './populateMadiTypeField')
 const stripOrphans = require('./stripOrphans')
 const buildSources = require('./buildSources')
-
+const customInspectSymbol = Symbol.for('nodejs.util.inspect.custom');
 const  {
     TAKEAWAY,
     PROBLEM,
@@ -11,35 +11,90 @@ const  {
     QUOTE,
     ACTOR,
     NONE,
-    CONST_ARRAY
+    CONST_ARRAY,
+    SOURCE_MATCH_ERROR,
+    SOURCE_MISSING_ERROR
 } = require('../constants')
 
 
 
 class Converter{
+    
     constructor(db ={}){
         if(Array.isArray(db) && db[0]['id']){
             this.db = {}
-            db.forEach(w=>{this.db[w.id]=w})
+            db.filter(w=>!!w.id).forEach(w=>this.db[w.id]=w)
         }else{
             this.db = db
         }
+        this.raw = Object.assign(this.db)
     }
-    populateMadiTypeField = populateMadiTypeField
-    addArrowRelations = addArrowRelations
+
+    addTypes = ()=>{
+        this.db = Object.values(this.db).map(w=>populateMadiTypeField(w))
+            .reduce((a, v) => (v.id?{ ...a, [v.id]: v}:a), {})
+    }
+
+    addRelations = ()=>{
+        this.arrows = Object.values(this.raw)
+            .filter(w=>w.type === 'arrow')
+            .map(a=>({endRefId:a.endRefId, startRefId:a.startRefId }))
+        
+        this.db = addArrowRelations(this.arrows, this.db)
+    }
+    findSourceErrors = ()=>{
+        Object.keys(this.db).map(wid=>{
+            let source = this.db[wid].source
+            if(!source){
+                this.db[wid].error = this.db[wid].error || []
+                this.db[wid].error.push(SOURCE_MISSING_ERROR)
+            }else{
+                (this.db[wid].children || []).forEach(cid=>{
+                    if(this.db[cid].source && this.db[cid].source !== this.db[wid].source){
+                        this.db[cid].error = this.db[cid].error || []
+                        this.db[cid].error.push(SOURCE_MATCH_ERROR)
+                    }
+                })
+            }
+        })
+    }
+    saveToFile = (filename,data)=>{
+        try {
+            fs.writeFileSync(filename, JSON.stringify(data,null,2));
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    convertToTree = ()=>{
+        let list = Object.values(this.db)
+        // list = list.slice(1,4)
+        // console.log(list)
+        const arrayToTree = (arr, parentId = 0) =>
+        arr.filter(item => {
+            let itemParent = !!item.parents ? item.parents[0] : 0;
+            return itemParent === parentId
+        }).map(child => ({ ...child, children: 
+            arrayToTree(arr, child.id )
+        }));
+
+        return arrayToTree(list)
+    }
+
     stripOrphans = stripOrphans
     buildSources = buildSources
-
 }
 
 
-let con = new Converter(widgets) 
-console.log(con)
 
-// let typed = populateMadiTypeField({}, raw)
-// let arranged = addArrowRelations(typed,raw)
-// let sourced = buildSources(arranged)
-// let { observations, orphans } = stripOrphans(sourced)
+
+let con = new Converter(widgets) 
+con.addTypes()
+con.addRelations()
+con.findSourceErrors()
+let tree = con.convertToTree()
+console.log(tree)
+
+
 
 // console.log({ observations, orphans })
 
