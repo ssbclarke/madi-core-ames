f17290206f54bcfef142be48cc017eeb363236a7
James Villarrubia <james.villarrubia@gmail.com>
2022-12-06 18:18:23 -0500

    obfuscates the test data

---
+++ mural-lib/test/hello.test.ts
@@ -0,0 +1,17 @@
+import { hello } from '../src/hello';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello function', () => {
+
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello world!');
+  });
+
+});
+
+describe('converter', () => {
+
+
+})
---
+++ mural-lib/test/obfuscator.js
@@ -0,0 +1,31 @@
+const { default: Obfuscator } = require('data-obfuscator/build/src/obfuscator');
+let json = require('./widgets.original.json')
+const fs = require('fs')
+
+
+
+let o = new Obfuscator()
+
+let keys = new Set()
+json.forEach(w=>{
+    Object.keys(w).forEach(k=>{
+        keys.add(k)
+    })
+})
+
+o.ignoredFields = keys;
+o.ignoredFields.delete('test')
+o.ignoredFields.add('yes')
+o.ignoredFields.delete("firstName");
+o.ignoredFields.delete("lastName");
+o.ignoredFields.delete("contentEditedBy")
+o.ignoredFields.delete("createdBy")
+o.ignoredFields.delete("updatedBy")
+o.ignoredFields.delete("hyperlink")
+o.ignoredFields.delete("text")
+let results = o.obfuscate(json);
+
+fs.writeFileSync("widgets.json",JSON.stringify(results))
+
+
+console.log(results)
